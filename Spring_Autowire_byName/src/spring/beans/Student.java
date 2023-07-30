d1e160c70847339157e7c2007d308e3a0733ac68
HP PC <HP PC@192.168.3.149>
2023-07-30 23:42:39 +0530

    Autowire byName

---
+++ Spring_Autowire_byName/src/spring/beans/Student.java
@@ -0,0 +1,35 @@
+package spring.beans;
+
+public class Student {
+	
+	private String name;
+	private int roll;
+	private Address address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	public void display() {
+		System.out.println("\nNAME:"+name+"\nROLL:"+roll);
+		System.out.println("CITY:"+address.getCity()+"--STATE:"+address.getState()+"--PIN:"+address.getPin());
+	}
+	
+	
+
+}
