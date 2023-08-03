6c00e6aedee7ab8e289a19388a3fd14071a0384b
HP PC <HP PC@192.168.3.149>
2023-08-03 21:32:48 +0530

    using objects

--- Spring_MethodReplacer/src/spring/beans/MethodRp.java
+++ Spring_MethodReplacer/src/spring/beans/MethodRp.java
@@ -9,7 +9,9 @@ public class MethodRp implements MethodReplacer {
 	@Override
 	public Object reimplement(Object arg0, Method arg1, Object[] arg2) throws Throwable {
 		// TODO Auto-generated method stub
+		
 		System.out.println("METHOD NAME:"+arg1.getName());
+		System.out.println("VAL:"+(Integer)arg2[0]);
 		return null;
 	}
 
